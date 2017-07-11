396ee7bffa91a4fc3bcc39042c731ddd6a5cbd61
wilsonianb <bwilson@ripple.com>
2017-07-11 13:11:58 -0700

    [WIP] Use games api

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }     from './dashboard.component';
@@ -14,6 +15,7 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
